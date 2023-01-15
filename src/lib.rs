62eb2884d1d55e12779bcb43a6ee3acc6cf95ea1
Christian Mauduit (ufoot) <ufoot@ufoot.org>
2023-01-15 22:03:54 +0100

    [doc] fixed icon URL

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! It offers a [partial order of events in a distributed system](https://en.wikipedia.org/wiki/Vector_clock).
 //! In practice, it implements [the rust partial order trait](https://doc.rust-lang.org/stable/std/cmp/trait.PartialOrd.html) over hash maps which maintain an integer count of each modification, per key.
 //!
-//! ![VClock icon](https://gitlab.com/liberecofr/vclock/raw/master/vclock.png)
+//! ![VClock icon](https://gitlab.com/liberecofr/vclock/raw/main/vclock.png)
 //!
 //! # Examples
 //!
